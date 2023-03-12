3f5bd66fad4e8453ee283850c121a0314da71003
Kashira2 <nekit.egorov@icloud.com>
2023-03-12 23:16:42 +0300

    dz2 3

--- dz2.java
+++ dz2.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+// Пусть дан произвольный список целых чисел, удалить из него чётные числа
+import java.util.Random;
+
 public class dz2 {
-    
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int x = rand.nextInt(100);
+            arr.add(x);
+        }
+        System.out.println(arr.toString());
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) % 2 == 0) {
+                arr.remove(i);
+                i--;
+            }
+        }
+
+        System.out.println(arr.toString());
+    }
 }
--- dz3.java
+++ dz3.java
@@ -1,3 +1,27 @@
+// Задан целочисленный список ArrayList. 
+// Найти минимальное, максимальное и среднее ариф. из этого списка.
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
 public class dz3 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int x = rand.nextInt(100);
+            arr.add(x);
+        }
+        System.out.println(arr.toString());
+
+        arr.sort(Comparator.naturalOrder());
+        Integer min = arr.get(0);
+        Integer max = arr.get(arr.size()-1);
+        Integer res = 0;
+        for (Integer integer : arr) {
+            res = res + integer;
+        }
+
+        System.out.printf("Максимальное число в списке = %s\nМинимальное число в спике = %s\nСреднее арифметическое всех чисел = %s", max, min, res/arr.size());
+    }
 }
